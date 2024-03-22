f84182c8fb0c36db4e1fd34b926d8890c837f670
jgrissom <jgrissom91@gmail.com>
2024-03-22 16:19:06 -0500

    [2.16a] Attach date picker to text field

--- balloons.js
+++ balloons.js
@@ -1,3 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-  console.log("hello world!");
+  const elem = document.getElementById('dob');
+  const datepicker = new Datepicker(elem);  
 });
