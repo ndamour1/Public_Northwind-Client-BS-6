00cdcd939ed5c18c35c9078304434e6956a18179
jgrissom <jgrissom91@gmail.com>
2024-03-22 14:07:05 -0500

    [2.06] Create "click" event listener to display toast

--- discount.js
+++ discount.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById('discount-row').addEventListener('click', function(e){
+    e.preventDefault();
     bootstrap.Toast.getOrCreateInstance(document.getElementById('liveToast')).show();
+  });
 });
