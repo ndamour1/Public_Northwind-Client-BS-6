56a0b5eccc8c3ae62fe8039cfa502c2d697a0eee
ndamour1 <ndamour1@my.wctc.edu>
2025-01-30 21:05:36 -0500

    Assignment 2 Code

--- discount.js
+++ discount.js
@@ -3,7 +3,21 @@ document.addEventListener("DOMContentLoaded", function() {
         if (e.target.classList.contains('discount')) {
             e.preventDefault();
             document.getElementById('product').innerHTML = e.target.dataset['product'];
+            document.getElementById('title').innerHTML = e.target.dataset['title'];
+            document.getElementById('code').innerHTML = e.target.dataset['code'];
             bootstrap.Toast.getOrCreateInstance(document.getElementById('liveToast')).show();
           }
       });
+    document.getElementById('discount-row').addEventListener('keyup', (event) => {
+        if (event.key === 'Escape') {
+            const toastElement = document.querySelector('discount-row');
+            if (toastElement) {
+                toastElement.classList.add('hide');
+            }
+        }
+        if (toastElement.classList.contains('hide'))
+            {
+                toast.hide();
+            }
+    });
 });
